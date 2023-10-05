5266dda3b449c51c3b4f14323c0931652c597c2f
Artur <imanbaevart123@gmail.com>
2023-10-06 02:01:09 +0300

    Added alert

--- PetApp/MapModule/Assembly/MapAssembly.swift
+++ PetApp/MapModule/Assembly/MapAssembly.swift
@@ -15,4 +15,16 @@ final class MapAssembly{
         let mapViewController = MapViewController(viewModel: mapViewModel)
         return mapViewController
     }
+    func createAlertMessage() -> UIAlertController{
+        let alert = UIAlertController(title: "Служба геолокации выключена", message: "Хотите включить?", preferredStyle: .alert)
+        let setting = UIAlertAction(title: "Настройки", style: .default) { alert in
+            if let url = URL(string: "App-Prefs:root=LOCATION_SERVICES"){
+                UIApplication.shared.open(url)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        alert.addAction(setting)
+        alert.addAction(cancelAction)
+        return alert
+    }
 }
